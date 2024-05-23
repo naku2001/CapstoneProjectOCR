e62aa04f803f130aba791c1857d517eb73045ab5
perfect.makuwerere@students.uz.ac.zw <angelsmakuwerere@gmail.com>
2024-05-23 07:59:35 +0200

    gemini

--- lib/Screen/recognization_page.dart
+++ lib/Screen/recognization_page.dart
@@ -1,7 +1,13 @@
 import 'dart:developer';
+import 'dart:typed_data';
 
+
+import 'package:google_generative_ai/google_generative_ai.dart';
 import 'package:flutter/material.dart';
 import 'package:google_mlkit_text_recognition/google_mlkit_text_recognition.dart';
+// import 'package:flutter_gemini/flutter_gemini.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'dart:io';
 
 class RecognizePage extends StatefulWidget {
   final String? path;
@@ -19,10 +25,11 @@ class _RecognizePageState extends State<RecognizePage> {
   @override
   void initState() {
     super.initState();
-
-    final InputImage inputImage = InputImage.fromFilePath(widget.path!);
-
-    processImage(inputImage);
+    _processImage();  // Call the asynchronous function
+  }
+  Future<void> _processImage() async { // Make this method asynchronous
+    final Uint8List imageBytes = await File(widget.path!).readAsBytes();
+    processImage(imageBytes);
   }
 
   @override
@@ -44,22 +51,47 @@ class _RecognizePageState extends State<RecognizePage> {
               ));
   }
 
-  void processImage(InputImage image) async {
-    final textRecognizer = TextRecognizer(script: TextRecognitionScript.latin);
+
+  void processImage(Uint8List imageBytes) async {
+    await dotenv.load(fileName: ".env"); // Load the .env file
+
+    // Access variables
+    String apiKey = dotenv.env['API_KEY']!;
+    // final textRecognizer = TextRecognizer(script: TextRecognitionScript.latin);
 
     setState(() {
       _isBusy = true;
     });
 
-    log(image.filePath!);
-    final RecognizedText recognizedText =
-        await textRecognizer.processImage(image);
+    // log(image.filePath!);
+    // final RecognizedText recognizedText =
+    //     await textRecognizer.processImage(image);
+    //
+    // controller.text = recognizedText.text;
 
-    controller.text = recognizedText.text;
+    // Generative AI Integration
+    final model = GenerativeModel(model: 'gemini-pro-vision', apiKey: apiKey);
+    //
+    // final imageBytes = await File(image.filePath!).readAsBytes(); // Load image bytes
+    // final imagePart = DataPart('image/jpeg', imageBytes);
+
+    final prompt = TextPart("extract text from the given image,keep structure of the text,if you identify tabular data draw a dataframe for the data");
+    // final content = Content.multi([prompt, imagePart]); // Combine text and image
+    final imagePart = DataPart('image/jpeg', imageBytes ); // Adjust MIME type if needed
+
+
+    final response = await model.generateContent([ Content.multi([prompt,imagePart])]);
+
+    // Combine results
+    controller.text =
+    "Extracted Text:\n${response.text}";
+
+
 
     ///End busy state
     setState(() {
       _isBusy = false;
     });
   }
+
 }
